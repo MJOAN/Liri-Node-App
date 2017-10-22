64013ea534bc9a1652164538d0d4da5a1fab92e8
Mariam Joan <MariamJoan@Mariams-MacBook-Pro.local>
2017-10-21 17:42:03 -0700

    10-21

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'ALburluBDwac42PXtE0oqwfC',
+  consumer_secret: 'gTwvY4XfTRpwr5gf4M2aND4aJ4W5s5hAWLnHJwJC6sNo0S0YYJ',
+  access_token_key: '919642726987001856-ZJPpygAuEQ0XmG7K2KG3lvfUmagbBIP',
+  access_token_secret: 'cJDrDE8QtWS9DHAJxSK9FT86d7JgYzvXuhrvk4OSO1I9G',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,43 @@
+
+// Client ID: 45663fbd48ef4e93ac6455142ea4ec38
+// Client secret: 229d8fb6643f4bd3bbe481282406ec5d
+/*
+At the top of the liri.js file, write the code you need to grab the data from keys.js. 
+Then store the keys in a variable.
+Make it so liri.js can take in one of the following commands:
+my-tweets
+spotify-this-song
+movie-this
+do-what-it-says
+
+
+Using the fs Node package, 
+LIRI will take the text inside of random.txt and 
+then use it to call one of LIRI's commands.
+It should run spotify-this-song for "I Want it That Way," as
+ follows the text in random.txt.
+Feel free to change the text in that document to 
+test out the feature for other commands.
+
+OMDB API: 40e9cece
+
+   * Title of the movie.
+   * Year the movie came out.
+   * IMDB Rating of the movie.
+   * Rotten Tomatoes Rating of the movie.
+   * Country where the movie was produced.
+   * Language of the movie.
+   * Plot of the movie.
+   * Actors in the movie.
+*/
+
+
+console.log('this is loaded');
+
+module.exports = twitterKeys;
+
+"my-tweets"
+"spotify-this-song"
+"movie-this"
+"do-what-it-says"
+
